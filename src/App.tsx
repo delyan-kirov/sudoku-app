9aebdce820f97d4a288b58004c66138a0dda066f
delyan-kirov <delyan.k.kirov@gmail.com>
2023-12-28 02:48:42 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const SudokuBoard: React.FC = () => {
+  const initialBoard = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9],
+  ];
+
+  const [board, setBoard] = useState<number[][]>(initialBoard);
+
+  const handleCellChange = (row: number, col: number, value: number) => {
+    const newBoard = [...board];
+    newBoard[row][col] = value;
+    setBoard(newBoard);
+  };
+
+  return (
+    <div className='SudokuBoard'>
+      <table>
+        <tbody>
+          {board.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cell, colIndex) => (
+                <td key={colIndex}>
+                  <input
+                    type="number"
+                    min="1"
+                    max="9"
+                    value={cell || ""}
+                    onChange={(e) =>
+                      handleCellChange(rowIndex, colIndex, +e.target.value)
+                    }
+                  />
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SudokuBoard />
       </header>
     </div>
   );
 }
 
 export default App;
+
